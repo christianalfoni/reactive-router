3ac09c12189de9c13a92bf471b0166095f6cc01a
Garth Williams <garth@wi.llia.ms>
2015-08-03 11:43:43 +0200

    Replaced the router internals with page.js

--- index.js
+++ index.js
@@ -1,70 +1,27 @@
-const Router = function (routes) {
+const page = require('page');
 
-  var silent = false;
-  var matchRoutes = function (currentUrl) {
-    var fragments = currentUrl.split('/');
-    var result = Object.keys(routes).reduce(function (info, url) {
-      var length = url.split('/').length;
-      if (Router.match(currentUrl, url) && info.hit > info.currentHit) {
-        info.cb = routes[url];
-        info.currentHit = length;
-        info.params = Router.match(currentUrl, url);
-      }
-      return info;
-    }, {
-      cb: null,
-      currentHit: 0,
-      params: null,
-      hit: fragments.length
-    });
-    result.cb({
-      url: currentUrl,
-      params: result.params,
-      fragments: fragments
-    });
-  };
+const Router = function (routes, options) {
 
-  window.addEventListener('hashchange', function () {
-    if (silent) {
-      silent = false;
-      return;
-    }
-    matchRoutes(location.hash.substr(1));
+  // register the routes
+  Object.keys(routes).map(function (route) {
+    page(route, function (context) {
+      context.url = context.path;
+      context.fragments = context.pathname.split('/');
+      routes[route](context);
+    });
   });
-  matchRoutes(location.hash ? location.hash.substr(1) : '/');
 
+  // start the router
+  page(options || {});
+
+  // export functions
   return {
     set: function (url) {
-      if (url !== location.hash.substr(1)) {
-        location.hash = url;
-      }
-    },
-    setSilent: function (url) {
-      if (url !== location.hash.substr(1)) {
-        silent = true;
-        location.hash = url;
+      if (page.current !== url) {
+        page(url);
       }
     }
   };
-
-};
-
-Router.match = function (currentUrl, url) {
-
-  var currentArray = currentUrl.split('/');
-  var array = url.split('/');
-
-  var params = {};
-
-  for (var x = 0; x < array.length; x++) {
-    if (array[x] !== currentArray[x] && array[x][0] !== ':') {
-      return null;
-    }
-    if (array[x][0] === ':' && currentArray[x]) {
-      params[array[x].substr(1)] = currentArray[x];
-    }
-  }
-  return params;
 
 };
 
