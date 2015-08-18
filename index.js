9ea287ab060da8b33876de154955f59f16f84a54
Brian Fitch <brianmfitch@gmail.com>
2015-08-18 10:14:42 -0400

    Map routes to URLs with RegExp

--- index.js
+++ index.js
@@ -1,34 +1,58 @@
 'use strict';
-var page = require('page');
+// var createHistory = require('history');
 
-var Router = function (routes, options) {
-
-  var isSilent = false;
-
-  // register the routes
-  Object.keys(routes).map(function (route) {
-    page(route, function () {
-      !isSilent && routes[route].apply(this, arguments);
-    });
-  });
-
-  // start the router
-  page(options || {});
-
-  // export functions
+var Router = function(routes) {
   return {
-    set: function (url) {
-      if (page.current !== url) {
-        page(url);
+    listen: function() {
+      this.listener = createHistory().listen(function(location) {
+        this._matchRoute(location);
+      });
+    },
+
+    stopListening: function() { this.listen.unlisten(); },
+
+    set: function(url) {
+      //todo
+    },
+
+    setSilent: function(url) {
+      this.isSilent = true;
+      this.set(url);
+      this.isSilent = false;
+    },
+
+    // "private" methods
+    _matchRoute: function(location) {
+      var route = this._findMatchingRoute(routes, location);
+      this._invokeCallback(routes, route, locaion);
+    },
+
+    _findMatchingRoute: function(routes, location) {
+      return (
+        Object.keys(routes).find(function(key) {
+          return this.match(key, location);
+        })
+      );
+    },
+
+    _match: function(route, location) {
+      return new RegExp(this._asRegex(route), 'g').test(location)
+    },
+
+    _asRegex: function(route) {
+      var regexableRoute = '^' + route.replace(/:(\w+)/g, "(:?\\w+)") + '$';
+      // console.log(regexableRoute);
+      return regexableRoute;
+    },
+
+    _invokeCallback: function(routes, route, location) {
+      if (!this.isSilent) {
+        return routes[route].call(this, location);
       }
     },
-    setSilent: function (url) {
-      isSilent = true;
-      this.set(url);
-      isSilent = false;
-    }
-  };
 
-};
+    isSilent: false
+  };
+}
 
 module.exports = Router;
--- tests/reactive-router-test.js
+++ tests/reactive-router-test.js
@@ -1,3 +1,62 @@
-exports['should '] = function () {
+var Router = require('../index');
 
+module.exports = {
+  setUp: function (callback) {
+    this.routes = {
+      '/':                  function(arg){ return 'rootCallback ' + arg; },
+      '/foo':               function(arg){ return 'fooCallback ' + arg; },
+      '/foo/:id':           function(arg){ return 'fooIdCallback ' + arg; },
+      '/foo/:id/baz':       function(arg){ return 'fooIdBazCallback ' + arg; },
+      '/foo/:id/baz/:guid': function(arg){ return 'fooIdBazGuidCallback ' + arg; }
+    };
+    this.router = Router(this.routes);
+    callback();
+  },
+
+  tearDown: function (callback) {
+    this.router = null;
+    callback();
+  },
+
+  _invokeCallback: function (test) {
+    callbackValue = this.router._invokeCallback(this.routes, '/foo', "location");
+
+    test.equal(callbackValue, 'fooCallback location');
+    test.done();
+  },
+
+  _match: function(test) {
+    // data
+    var routesFixture = {
+      '/': '/',
+      '/foo': '/foo',
+      '/foo/bar': '/foo/bar',
+      '/foo/:id': '/foo/123',
+      '/foo/:id/baz': '/foo/123/baz',
+      '/foo/:id/baz/:guid': '/foo/123/baz/abc'
+    };
+    var routes = Object.keys(routesFixture);
+
+    // assertion helper
+    var self = this;
+    function assertMatch(route, location, nonMatchingRoutes) {
+      test.ok(self.router._match(route, location));
+
+      nonMatchingRoutes.forEach(function(route, location) {
+        test.ok(!self.router._match(route, location));
+      });
+    }
+
+    // test
+    routes.forEach(function(route) {
+      var mutableRoutes  = routes.slice()
+      var index          = mutableRoutes.indexOf(route);
+      var routeUnderTest = mutableRoutes.splice(index, 1).shift();
+      var location       = routesFixture[route];
+
+      assertMatch(routeUnderTest, location, mutableRoutes);
+    });
+
+    test.done();
+  }
 };
